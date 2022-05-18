649bfe15a11362e455102199215bd99128ea744d
Бояршинов Константин <costya74@bk.ru>
2022-05-18 20:46:21 +0500

    commit cod

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -5,13 +5,14 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import lombok.Value;
 
 import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
 
 public class DataGenerator {
+
+    private static final Faker faker = new Faker(new Locale("en"));
     private static final RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("http://localhost")
             .setPort(9999)
@@ -19,52 +20,55 @@ public class DataGenerator {
             .setContentType(ContentType.JSON)
             .log(LogDetail.ALL)
             .build();
-    private static final Faker faker = new Faker(new Locale("en"));
 
     private DataGenerator() {
     }
 
-    private static void sendRequest(RegistrationDto user) {
-        //Запрос
-        given() //"дано"
-                .spec(requestSpec) //Указывается, какая спецификация используется
-                .body(new RegistrationDto( //Передача в теле объекта, который будет преобразован в JSON,
-                        user.getLogin(),        //собственно логин,
-                        user.getPassword(),     //пароль
-                        user.getStatus()))      //и статус.
-                .when()                         //"когда"
-                .post("/api/system/users") //На какой путь, относительно BaseUri отправляется запрос
-                .then()                         //"тогда ожидаем"
-                .statusCode(200);               //Код 200, все хорошо
-    }
-
-    public static String getRandomLogin() {
-        return faker.name().firstName();
-    }
-
-    public static String getRandomPassword() {
-        return faker.internet().password();
+    static void sendQuery(UserInfo userInfo) {
+        // сам запрос
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(userInfo) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
     }
 
     public static class Registration {
         private Registration() {
         }
 
-        public static RegistrationDto getUser(String status) {
-            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+        public static String generateLogin() {
+            return faker.name().username();
         }
 
-        public static RegistrationDto getRegisteredUser(String status) {
-            RegistrationDto getRegisteredUser = getUser(status);
-            sendRequest(getRegisteredUser);
-            return getRegisteredUser;
+        public static String generatePassword() {
+            return faker.internet().password();
         }
-    }
 
-    @Value
-    public static class RegistrationDto {
-        String login;
-        String password;
-        String status;
+        public static UserInfo generateValidUser() {
+            UserInfo userInfo = new UserInfo(generateLogin(), generatePassword(), "active");
+            sendQuery(userInfo);
+            return userInfo;
+        }
+
+        public static UserInfo generateBlockedUser() {
+            UserInfo userInfo = new UserInfo(generateLogin(), generatePassword(), "blocked");
+            sendQuery(userInfo);
+            return userInfo;
+        }
+
+        public static UserInfo generateWrongPasswordUser(String status) {
+            String login = generateLogin();
+            sendQuery(new UserInfo(login, generatePassword(), status));
+            return new UserInfo(login, generatePassword(), status);
+        }
+
+        public static UserInfo generateWrongLoginUser(String status) {
+            String password = generatePassword();
+            sendQuery(new UserInfo(generateLogin(), password, status));
+            return new UserInfo(generateLogin(), password, status);
+        }
     }
 }
